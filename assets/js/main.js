ea9ca2b7a17d52aa98f55c8dfef92edac28b4bca
Michaeljacobej <michaeljacob6789@gmail.com>
2022-11-20 01:37:57 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -292,16 +292,16 @@
 				var $this = $(this);
 
 				// Close.
-					$('<div class="close">Close</div>')
-						.appendTo($this)
-						.on('click', function() {
-							location.hash = '';
-						});
+					// $('<div class="close">Close</div>')
+					// 	.appendTo($this)
+					// 	.on('click', function() {
+					// 		location.hash = '';
+					// 	});
 
 				// Prevent clicks from inside article from bubbling.
-					$this.on('click', function(event) {
-						event.stopPropagation();
-					});
+					// $this.on('click', function(event) {
+					// 	event.stopPropagation();
+					// });
 
 			});
 
